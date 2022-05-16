7c12c410a961bc937c680a00f2e1a37c82e8ce47
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-05-16 14:31:23 +0300

    Update lab2.js

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -17,3 +17,17 @@ function factorial(n){
    else 
      return 1;
   }
+
+ function fib(n) {
+  let a = 1n;
+  let b = 1n;
+ if (n==0) return 0;
+ if (n==1) return 1;
+ if (n==2) return 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}

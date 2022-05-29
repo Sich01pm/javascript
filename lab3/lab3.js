13fb739ea1586ef38a405702414e9ee15223cea8
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-05-29 15:08:11 +0300

    n3

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,10 +1,16 @@
 'use strict';
 function getDecimal(num) {
-        let a = num.slice(num.indexOf("."));
-        let str = 0 + a;
-        if (Number(num) < 0) {
-            str = 1 - Number(str);
-            str = str.toFixed(a.length - 1);
-        }
-        return str;
+    let a = num.slice(num.indexOf("."));
+    let str = 0 + a;
+    if (Number(num) < 0) {
+        str = 1 - Number(str);
+        str = str.toFixed(a.length - 1);
+    }
+    return str;
 }
+
+function ucFirst(str) {
+    return (str[0].toUpperCase() + str.slice(1));
+}
+let str = prompt("Введи строку");
+alert(ucFirst(str));

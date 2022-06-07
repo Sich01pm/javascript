1fb57c777c431af99ab7c25a21ff93da1835b47a
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-06-07 23:24:59 +0300

    comments

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,8 +1,18 @@
 'use strict';
+/**
+ * 
+ * @param {Number} x-основание степени
+ * @param {Number} n-показатель степени
+ * @returns возводит в степень x^n
+ */
 function pow(x,n){
   return x**n;  
 }
-
+/**
+ * 
+ * @param {Number} n - количество чисел
+ * @returns возвращает сумму от 1 до N
+ */
 function sumTo(n){
   let sum=0;
   for(let i=1;i<=n;i++){
@@ -10,14 +20,22 @@ function sumTo(n){
   }
   return sum;
 }
-
+/**
+ * 
+ * @param {Number} n - основание факториала 
+ * @returns возвращает n-ый факториал
+ */
 function factorial(n){
  if(n>0)
    return (n*factorial(n-1));
    else 
      return 1;
   }
-
+/**
+ * 
+ * @param {Number} n - ое число число Фибоначчи
+ * @returns возвращает n-oe число Фибоначчи
+ */
  function fib(n) {
   let a = 1n;
   let b = 1n;
@@ -31,7 +49,11 @@ function factorial(n){
   }
   return b;
 }
-
+/**
+ * 
+ * @param {Number} x - одно из ставниваемых чисел
+ * @returns функцию
+ */
 function compare(x){
   let res=function(){
     let y=prompt('Введите число y');
